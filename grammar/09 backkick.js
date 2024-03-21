bd7b3f9082ae68b463bf1203ed53d03c00f782c7
hyorimKang <hyoggang0327@gmail.com>
2024-03-21 16:06:32 +0900

    nullish coalescing

---
+++ grammar/09 backkick.js
@@ -0,0 +1,8 @@
+const dollar =  1323.69
+const yen = 8.76
+
+console.log('1달러 : ', + dollar + '원\n'
++ '1엔 : ' + yen + '원')
+
+//backkick을 사용하면 더 직관적이고 편함
+console.log(`1달러 : ${dollar}원\n1엔 : ${yen}원`)
