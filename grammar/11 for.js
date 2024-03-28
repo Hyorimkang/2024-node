8d87c90a16432d0fd9e8793a36435367ba429366
hyorimKang <hyoggang0327@gmail.com>
2024-03-28 14:51:53 +0900

    파일위치변경

---
+++ grammar/11 for.js
@@ -0,0 +1,14 @@
+const fruit = ['apple', 'banana', 'orange', 'pear']
+
+// for(let i = 0; i<fruit.length; i++){
+//     console.log(fruit[i])
+
+// }
+
+
+// fruit.forEach(f => console.log(f))
+
+
+for(const f of fruit){
+    console.log(f)
+}
