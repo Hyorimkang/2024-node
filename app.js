2e66db47d0864a44ba6c58364d8f47252d2b5d8b
hyorimKang <hyoggang0327@gmail.com>
2024-04-01 11:28:23 +0900

    express static

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
+const path = require('path')    //node에서 제공하는 기본패키지
 const app = express();
 const port = 3000;
 
+
+app.use('/html', express.static(path.join(__dirname, 'html')))
 
 // get method 요청에 대한 라우팅
 app.get('/', (req, res) => {
     res.send("hello world")
 })
 
-app.get('/cute', (req, res) => {
-    res.send("cute")
-})
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
+    console.log(express.static(path.join(__dirname, 'html')))
 })
