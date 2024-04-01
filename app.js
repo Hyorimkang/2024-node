8c098653bf92bb7f91f24e13eb234baec303b752
hyorimKang <hyoggang0327@gmail.com>
2024-04-01 11:52:08 +0900

    express parameter 정의

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const port = 3000;
 
 
-app.use('/html', express.static(path.join(__dirname, 'html')))
+// app.use('/html', express.static(path.join(__dirname, 'html'))) 
+app.use('/html', express.static(__dirname + '/html'))
 
 // get method 요청에 대한 라우팅
 app.get('/', (req, res) => {
@@ -12,7 +13,17 @@ app.get('/', (req, res) => {
 })
 
 
+
+//http://localhost:3000/test/KHR?lang=ko
+app.get('/test/:name', (req, res) =>{
+    console.log('path', req.path)       //'test/KHR'
+    console.log('params', req.params)   //{name: 'KHR'}
+    console.log('query', req.query)     //{lang: 'ko'}
+    res.send('콘솔 보세요')
+
+})
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
-    console.log(express.static(path.join(__dirname, 'html')))
+    console.log(path.join(__dirname, 'html'))
 })
